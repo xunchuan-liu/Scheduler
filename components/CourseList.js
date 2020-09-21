9388fe20b4306c5455891fabf723a990ac76780b
therodrammer <39782635+therodrammer@users.noreply.github.com>
2020-09-21 12:26:39 -0500

    Interactive Filtering done

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
 
 import Course from './Course';
+import TermSelector from './TermSelector';
 
 
-const CourseList = ({courses}) => (
-    <ScrollView>
-      <View style={styles.courseList}>
-        {courses.map(course => <Course key={course.id} course={course}/>)}
-      </View>  
-    </ScrollView>
+const termMap = { 
+    F: 'Fall',
+    W: 'Winter',
+    S: 'Spring'
+};
+const terms = Object.values(termMap);
+
+const getCourseTerm = course => termMap[course.id.charAt(0)];
+
+
+const CourseList = ({courses}) => {
+    const [selectedTerm, setSelectedTerm] = useState('Fall');
+    const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));    
+    
+    return (
+        <ScrollView>
+            <TermSelector terms={terms} selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm}/>                        
+            <View style={styles.courseList}>
+                { termCourses.map(course => <Course key={course.id} course={course}/>)}
+            </View>            
+        </ScrollView>
+        
   );
+}
 
 const styles = StyleSheet.create({    
     courseList: {
