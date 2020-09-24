5980641b81344cf9fb6a0b90504a729206bcff41
therodrammer <39782635+therodrammer@users.noreply.github.com>
2020-09-24 10:14:18 -0500

    Course Editing done

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import UserContext from './UserContext';
+
 import ScheduleScreen from './screens/ScheduleScreen';
 import CourseDetailScreen from './screens/CourseDetailScreen';
+import CourseEditScreen from './screens/CourseEditScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [user, setUser] = useState({ role: 'admin'});
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name='ScheduleScreen'
-          component={ScheduleScreen}
-          options={{ title: 'Schedule' }}
-        /> 
-        <Stack.Screen name='CourseDetailScreen'
-          component={CourseDetailScreen}
-          options={{ title: 'Course Detail' }}
-        />               
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserContext.Provider value={user}>
+      <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name='ScheduleScreen'
+            component={ScheduleScreen}
+            options={{ title: 'Schedule' }}
+          /> 
+          <Stack.Screen name='CourseDetailScreen'
+            component={CourseDetailScreen}
+            options={{ title: 'Course Detail' }}
+          />
+          <Stack.Screen name='CourseEditScreen'
+            component={CourseEditScreen}
+            options={{ title: 'Course Editor' }}     
+          />          
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserContext.Provider>
   );
 };
 
--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
+
+import UserContext from '../UserContext';
 
 import CourseList from '../components/CourseList';
 
@@ -10,10 +12,13 @@ const Banner = ({title}) => (
 
 
 const ScheduleScreen = ({navigation}) => {
+  const user = useContext(UserContext);  
+  const canEdit = user && (user.role === 'admin');
+  
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
 
   const view = course => {
-      navigation.navigate('CourseDetailScreen', { course });
+      navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
   };
 
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
